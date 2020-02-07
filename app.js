998d681f16fc7185b11fe3293137a05d8e2ee9b6
rkiyuna <rkeeyuna@gmail.com>
2020-02-07 14:30:44 -0800

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+//Javascript begin
+var pics = [
+    "imgs/kitty_bed.JPG",
+    "imgs/kitty_basket.JPG",
+    "imgs/kitty_laptop.jpg",
+    "imgs/kitty_door.jpg",
+    "imgs/kitty_sink.JPG",
+    "imgs/kitty_wall.jpg"
+]
